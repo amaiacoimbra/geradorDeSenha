794a39281dbd897ac3c3c0bcb5d1567c3617d2a1
Thainunes <thaissnunes@gmail.com>
2022-03-03 20:14:18 -0300

    Finalização do projeto Gerador de Senha

--- script.js
+++ script.js
@@ -12,10 +12,23 @@ let novaSenha = "";
 
 sizePassword.innerHTML = sliderElement.value;
 
-slider.oninput = function(){
+slider.oninput = function () {
     sizePassword.innerHTML = this.value
 }
 
-function generatePassoword(){
-    alert("VAMOS GERAR A SENHA")
+function generatePassword() {
+
+    let pass = "";
+    for (let i = 0, n = charset.length; i < sliderElement.value; i++) {
+        pass += charset.charAt(Math.floor(Math.random() * n))
+    }
+
+    containerPassword.classList.remove("hide");
+    password.innerHTML = pass;
+    novaSenha = pass;
+}
+
+function copyPassword() {
+    alert("Senha copiada com sucesso!");
+    navigator.clipboard.writeText(novaSenha);
 }
